95bef8da1876e318dcbf87c432ac837f005ec23d
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-01-09 15:20:47 -0500

    merge

---
+++ game.rb
@@ -0,0 +1,32 @@
+p "What is your hero's name?"
+hero = gets.chomp
+p "Welcome #{hero}, prepare for battle."
+
+party = {
+  "hero" => [10]
+}
+
+
+npc = {
+  "enemy" => [10]
+}
+
+while party["hero"] > 0
+
+  p "A #{enemy} is approaching. (Will you \"fight\" or \"flee\")"
+
+
+  if gets.chomp.downcase == "flee"
+    party["hero"]  = party["hero"] - 2
+
+
+
+
+
+
+
+
+  end
+
+  puts "you died fucker"
+  
