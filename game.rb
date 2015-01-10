fd4edcb6539aa7152a926e473783da20683e21b4
TylerNodell <Tyler.L.Nodell@gmail.com>
2015-01-10 10:54:24 -0500

    Error on line 73

--- game.rb
+++ game.rb
@@ -14,6 +14,10 @@ def battle(party, npc)
 
   totalhp = party["hero"][0]
   enemyhp = npc["enemy"][0]
+  hstr = party["hero"][1]
+  estr = npc["enemy"][1]
+  hdef = party["hero"][2]
+  edef = npc["enemy"][2]
 
   while party["hero"][0] > 0 || npc["enemy"][0] > 0
     p "How will you proceed? Attack, block or run?"
@@ -23,16 +27,11 @@ def battle(party, npc)
       if 1+rand(5) == 1
         p "Your attack missed."
       elsif 1+rand(5) != 1
-         edamage = 2 + rand(3)
-        p "You hit for #{edamage} damage!"
+        edamage = 1 + rand(hstr)
       end
-
-      if 1+rand(5) <= 2
-        p "The enemies attack misses!"
-      elsif 1+rand(5) >= 3
-        hdamage = 1 + rand(3)
-        p "You've been hit for #{hdamage} damage!"
-      end
+    elsif input == "block"
+      hdef = hdef *2
+      p "You are blocking."
     elsif input == "run"
       party["hero"][0] = party["hero"][0] - 2
       p "You escape! You have lost 2 health during your escape!"
@@ -42,6 +41,46 @@ def battle(party, npc)
       # story(party,npc)
     end
 
+    if npc["enemy"][0] > enemyhp/2
+      if 1 + rand(10) == 10
+        edef = edef * 2
+      else
+        if 1+rand(5) <= 2
+        p "The enemies attack misses!"
+        elsif 1+rand(5) >= 3
+          hdamage = 1 + rand(estr)
+        end
+      end
+    else
+      if 1 + rand(2) == 1
+        eblock = edef * 2
+      else
+        if 1+rand(5) <= 2
+          p "The enemies attack misses!"
+        elsif 1+rand(5) >= 3
+          hdamage = 1 + rand(estr)
+        end
+      end
+
+      edamage = edamage - edef
+          if edamage <= 0
+            p "Your attack failed!"
+            edamage = 0
+          else
+            p "You hit for #{edamage} damage!"
+          end
+
+      hdamage = hdamage - hdef
+          if hdamage <= 0
+            p "The enemies attack has failed!"
+            hdamage = 0
+          else
+            p "You've been hit for #{hdamage} damage!"
+          end
+
+    end
+
+    
     party["hero"][0] = party["hero"][0] - hdamage.to_i
     npc["enemy"][0] = npc["enemy"][0] - edamage.to_i
     p "You have #{party["hero"][0]} health."
@@ -49,7 +88,7 @@ def battle(party, npc)
 
     if npc["enemy"][0] <= 0
       p "You have defeated the enemy!"
-      party["hero"][0] = totalhp + 1
+      party["hero"][0] = totalhp
       npc["enemy"][0] = enemyhp
       story(party, npc)
     end
