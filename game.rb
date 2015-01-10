ffed5905d3070cf81b5db054c74071e52e3281e6
TylerNodell <Tyler.L.Nodell@gmail.com>
2015-01-10 10:19:42 -0500

    fully working basics

--- game.rb
+++ game.rb
@@ -1,44 +1,46 @@
 # require 'pry'
 
 party = {
-  "hero" => [10]
+  "hero" => [10,5,5]
 }
 
 
 npc = {
-  "enemy" => [10],
+  "enemy" => [6,4,4],
   "name" => ["Tyler","Sean"]
 }
 
 def battle(party, npc)
 
+  totalhp = party["hero"][0]
+  enemyhp = npc["enemy"][0]
+
   while party["hero"][0] > 0 || npc["enemy"][0] > 0
-    p "How will you proceed? Attack or run?"
+    p "How will you proceed? Attack, block or run?"
     input = gets.chomp.downcase
 
-    if input == "attack" && 1+rand(5) == 1
+    if input == "attack"
+      if 1+rand(5) == 1
         p "Your attack missed."
-    elsif input == "attack" && 1+rand(5) != 1
-       edamage = 2 + rand(3)
-      p "You hit for #{edamage} damage!"
-    end
+      elsif 1+rand(5) != 1
+         edamage = 2 + rand(3)
+        p "You hit for #{edamage} damage!"
+      end
 
-    if input == "attack" && 1+rand(5) <= 2
-      p "The enemies attack misses!"
-    elsif input == "attack" && 1+rand(5) >= 3
-      hdamage = 1 + rand(3)
-      p "You've been hit for #{hdamage} damage!"
-    end
-
-    if input == "run"
+      if 1+rand(5) <= 2
+        p "The enemies attack misses!"
+      elsif 1+rand(5) >= 3
+        hdamage = 1 + rand(3)
+        p "You've been hit for #{hdamage} damage!"
+      end
+    elsif input == "run"
       party["hero"][0] = party["hero"][0] - 2
+      p "You escape! You have lost 2 health during your escape!"
       story(party,npc) #should initiate story fuction
+    elsif input != "run" || input != "attack"
+      p "That is an inproper command try again."
+      # story(party,npc)
     end
-
-    # if input != "run" || input != "attack"
-    #   p "That is an inproper command try again."
-    #   story(party,npc)
-    # end
 
     party["hero"][0] = party["hero"][0] - hdamage.to_i
     npc["enemy"][0] = npc["enemy"][0] - edamage.to_i
@@ -47,6 +49,8 @@ def battle(party, npc)
 
     if npc["enemy"][0] <= 0
       p "You have defeated the enemy!"
+      party["hero"][0] = totalhp + 1
+      npc["enemy"][0] = enemyhp
       story(party, npc)
     end
 
@@ -59,12 +63,12 @@ def story(party, npc)
 
   while party["hero"][0] >= 1
 
-    p "A #{npc["name"][0]} is approaching. (Will you \"fight\" or \"flee\")"
+    p "A #{npc["name"][1+rand(2)]} is approaching. (Will you \"fight\" or \"flee\")"
 
     input = gets.chomp.downcase
 
     if input == "flee"
-      party["hero"]  = party["hero"] - 2
+      party["hero"][0]  = party["hero"][0] - 2
 
     elsif input == "fight"
       p "You engage the enemy!"
